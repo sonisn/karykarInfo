ac15dae598d4643c7a199d38928b17a1f968ae88
sonisn <soni.snehal@gmail.com>
2021-02-21 20:05:30 -0600

    Create karyakarInfo.py
    Initial code for ims karyakar info scrapper.

---
+++ karyakarInfo.py
@@ -0,0 +1,27 @@
+from requests import Session
+from bs4 import BeautifulSoup as bs
+
+url = "https://ims.na.baps.org/Login.aspx"
+loginPage = "/Login.aspx"
+loginHandler = "https://ims.na.baps.org/LoginHandler.ashx?action=chklogin&Rnd=1616345598272"
+midwestKaryakar = "https://ims.na.baps.org/KaryakarHandler.ashx?action=getstructurekaryakar&Rnd=1616355822768"
+midwestFormData = {"txtStructureId": '9c1498bf-c6e5-476c-8498-4b34c3f09363', "txtEntityId": '52408f3c-1587-498f-b688-061eda7c1a0f', "isReadOnly": 'No'}
+homePage = "/Home.asps"
+karyakarPage = "/Karyakar.aspx"
+username = "<<username>>"
+password = "<<password>>"
+
+with Session() as session:
+    session.get(url + loginPage)
+    login_data = {'txtLogin': username, 'txtPassword': password}
+    session.post(loginHandler, data=login_data)
+    session.get(url + homePage)
+    session.get(url + karyakarPage)
+    site = session.post(midwestKaryakar, data=midwestFormData)
+    soup = bs(site.content, 'html.parser')
+    karyakarTable = soup.find(id='gridKaryakar')
+    for eachRow in karyakarTable.find_all('tr'):
+        for eachColumn in eachRow.find_all('td'):
+            print(eachColumn.text, end='')
+        print()
+
